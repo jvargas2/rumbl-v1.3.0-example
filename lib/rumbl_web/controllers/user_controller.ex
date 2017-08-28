2429e6acc246fcf233545c2652ccc54fcf7597b1
Jorge Vargas <jvargas2@icloud.com>
2017-08-27 22:27:08 -0400

    List all users

---
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -0,0 +1,11 @@
+defmodule RumblWeb.UserController do
+	use RumblWeb, :controller
+
+	alias Rumbl.User
+	alias Rumbl.Repo
+
+	def index(conn, _params) do
+		users = Repo.all(User)
+		render conn, "index.html", users: users
+	end
+end
