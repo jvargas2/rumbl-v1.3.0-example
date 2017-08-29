5a68c2d5637604d5738e058245197d71e5bbf02f
Jorge Vargas <jvargas2@icloud.com>
2017-08-28 21:33:15 -0400

    Finish ecto chapter

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,36 +1,11 @@
-# defmodule Rumbl.Repo do
-#   use Ecto.Repo, otp_app: :rumbl
-
-#   @doc """
-#   Dynamically loads the repository url from the
-#   DATABASE_URL environment variable.
-#   """
-#   def init(_, opts) do
-#     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-#   end
-# end
-
 defmodule Rumbl.Repo do
+  use Ecto.Repo, otp_app: :rumbl
 
-  @moduledoc """
-  In memory Repository
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
   """
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
-
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
-     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
-  end
-  def all(_module), do: []
 end
--- lib/rumbl/user.ex
+++ lib/rumbl/user.ex
@@ -1,3 +1,20 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Rumbl.User
+
+  schema "users" do
+  	field :name, :string
+  	field :username, :string
+  	field :password, :string, virtual: true
+  	field :password_hash, :string
+
+  	timestamps()
+  end
+
+  def changeset(user, params \\ %{}) do
+  	user
+  	|> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
