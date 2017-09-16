6a5bd8e906f425123c1535e71e001895a324318c
Jorge Vargas <jvargas2@icloud.com>
2017-09-16 16:48:21 -0400

    Finish watching videos chapter

--- lib/rumbl/content/video.ex
+++ lib/rumbl/content/video.ex
@@ -3,13 +3,14 @@ defmodule Rumbl.Content.Video do
   import Ecto.Changeset
   alias Rumbl.Content.Video
 
-
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
     field :url, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
+    field :slug, :string
 
     timestamps()
   end
@@ -22,6 +23,27 @@ defmodule Rumbl.Content.Video do
     video
     |> cast(attrs, @required_fields, @optional_fields)
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Content.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
