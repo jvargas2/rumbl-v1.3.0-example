c332c2fa1b2d27d411b3c683bbe97ac12edbf2f4
Jorge Vargas <jvargas2@icloud.com>
2017-09-16 17:55:33 -0400

    Correctly set up joinable channel

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
